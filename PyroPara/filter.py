141cafd627a4d1ff51d390d2a06665c79f300dd3
Krystof Majer <majer.krystof6375@gmail.com>
2021-12-19 14:36:34 +0100

    stafile & filter class added

---
+++ PyroPara/filter.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+from os import chdir
+from os.path import dirname
+import pandas as pd
+from pandas.core.frame import DataFrame
+import numpy as np
+from scipy import signal
+from scipy.signal import argrelextrema
+
+
+class Filter:
+    pass
