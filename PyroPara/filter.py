11617d88b34bfd6d28110c55bdfa8ac69c93c91a
Krystof Majer <majer.krystof6375@gmail.com>
2021-12-20 15:42:13 +0100

    first attempt in proper OOP

--- PyroPara/filter.py
+++ PyroPara/filter.py
@@ -1,12 +1,39 @@
-import matplotlib.pyplot as plt
-from os import chdir
-from os.path import dirname
 import pandas as pd
-from pandas.core.frame import DataFrame
 import numpy as np
 from scipy import signal
-from scipy.signal import argrelextrema
 
 
-class Filter:
-    pass
+""" 
+Class for defining filters to by used on STAfila class during process method
+
+args
+
+type  = type of window of filter
+cutoff = frequency at which to start the filter
+winsize =  size of filter window
+"""
+
+# TODO: place checks and confirmations to improve stability. Write tests
+
+
+class Filters:
+    def __init__(self, name: str, type: str, cutoff: int, winsize: int):
+        self.type = type
+        self.cutoff = cutoff
+        self.winsize = winsize
+        self.name = name
+
+    def f_sample(self, x: int):
+        """
+        Takes in the time axis of data and samples the freqency
+        """
+        self.sample = 1 / (x[1] - x[0])
+        return self.sample
+
+    def filt(self, y: int):
+
+        self.sample = Filters.f_sample()
+        w = signal.firwin(
+            self.winsize, self.cutoff / (0.5 * self.sample), window=self.type
+        )
+        return signal.filtfilt(w, 1, y)
--- PyroPara/stafile.py
+++ PyroPara/stafile.py
@@ -1,10 +1,13 @@
 import numpy as np
 import pandas as pd
+from filter import Filters
+
+# TODO: make sure the approach is not dead end. Write tests
 
 
 class STAfile:
-    number_of_files = 0
     list_of_files = []
+    dict_of_filters = {}
 
     def __init__(self) -> None:
         self._df = None
@@ -27,20 +30,34 @@ class STAfile:
         self._df.temperature += 273.15
         self._df.mass /= 100
 
-        STAfile.number_of_files += 1
         STAfile.list_of_files.append(path)
         return self._df
 
-    def process(self):  # Need access to filter function!!!
-        """Derivatves of mass data series"""
+    def add_filter(self, filter_to_add):
+        """adds Filters class instance to dict paired with is name attribute as key"""
+        if isinstance(filter_to_add, Filters):
+            STAfile.list_of_filters.update(filter_to_add.name, filter_to_add)
+
+    def process(self, filter_to_use: str):
+        """
+        Differentiation of mass data series
+
+        args:
+        filter_to_use: key string
+            indicates which filter to use forom list filters added to STAfile instance
+
+        """
+        filter_in_use = STAfile.dict_of_filters[filter_to_use]
 
         # 1. TG
-        self._df["mass_filtered"] = filter(self._df.time, self._df.mass)
+        self._df["mass_filtered"] = filter_in_use.filt(
+            self._df.time, self._df.mass
+        )
         # 2. DTG
         self._df["mass_diff_unfiltered"] = -np.gradient(
             self._df.mass_filtered, self._df.time
         )
-        self._df["mass_diff_filtered"] = filter(
+        self._df["mass_diff_filtered"] = filter_in_use.filt(
             self._df.time, self._df.mass_diff_unfiltered
         )
         # 3. DDTG
@@ -48,6 +65,5 @@ class STAfile:
             np.gradient(self._df.mass_diff_filtered, self._df.time)
         )
         self._df["mass_diff2_filtered"] = abs(
-            filter(self._df.time, self._df.mass_diff2_unfiltered)
+            filter_in_use.filt(self._df.time, self._df.mass_diff2_unfiltered)
         )
-        return self._df_processed
