94da8be49c5177fd20acfa2e3475731f4d9c396a
Jiri Ira <ira@numsolution.cz>
2021-12-23 16:23:54 +0100

    Add menu bar

--- PyroPara/gui/windows.py
+++ PyroPara/gui/windows.py
@@ -1,10 +1,31 @@
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QAction, QKeySequence
 from PySide6.QtWidgets import QMainWindow  # type: ignore
 
 from PyroPara import __version__
 
 
 class MainWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.setWindowTitle(f"PyroPara {__version__}")
         self.resize(1024, 768)
+
+        self.menu_bar = self.menuBar()
+        self.open_menu_action: QAction
+
+        self.setup_ui()
+
+    def setup_ui(self) -> None:
+        self.create_menus()
+
+    def create_menus(self) -> None:
+        self.create_file_menu()
+
+    def create_file_menu(self) -> None:
+        file_menu = self.menu_bar.addMenu("&File")
+
+        self.open_menu_action = QAction("Open...", self)
+        self.open_menu_action.setShortcut(QKeySequence(Qt.CTRL + Qt.Key_O))
+
+        file_menu.addAction(self.open_menu_action)
