80211e969b501ee81fdb619cbe097d278516e61c
Krystof Majer <majer.krystof6375@gmail.com>
2022-03-25 00:45:33 +0100

    ui refactoring, add run button

--- PyroPara/gui/windows.py
+++ PyroPara/gui/windows.py
@@ -2,8 +2,14 @@ from os.path import join
 
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QAction, QIcon, QKeySequence
-from PySide6.QtWidgets import QMainWindow  # type: ignore
-from PySide6.QtWidgets import QListWidget, QSplitter
+from PySide6.QtWidgets import (
+    QMainWindow,
+    QListWidget,
+    QSplitter,
+    QPushButton,
+    QVBoxLayout,
+)
+
 
 from PyroPara import BASE_DIR, __version__
 from PyroPara.gui.base import PlotWidget
@@ -20,29 +26,54 @@ class MainWindow(QMainWindow):
         self.resize(1024, 768)
 
         self.main_widget: QSplitter
+        self.button_widget: QPushButton
 
-        self.sta_files_widget = QListWidget()
-        self.menu_bar = self.menuBar()
-        self.read_menu_action: QAction
+        self.L_R_layout = QVBoxLayout()
+
+        self.create_left_panel()
+        self.create_right_panel()
 
         self.setup_ui()
 
     def setup_ui(self) -> None:
+        # setting up main widget
         self.main_widget = QSplitter(Qt.Horizontal)
         self.main_widget.setChildrenCollapsible(False)
         self.setCentralWidget(self.main_widget)
 
+        # setting up left panel
+        self.sta_files_widget = QListWidget()
+        self.menu_bar = self.menuBar()
+        self.run_button = QPushButton("Run")
+        self.left_panel.addWidget(self.sta_files_widget)
+        self.left_panel.addWidget(self.run_button)
+
+        # setting up middle panel
         self.plot_widget = PlotWidget()
         self.plot_widget.grid(visible=True)
         self.plot_widget.plot((0, 0.5, 1, 0), (0, 0.5, 0, 0), "r-")
 
-        self.main_widget.addWidget(self.sta_files_widget)
+        # setting up right panel
+        self.local_minima_list = QListWidget()
+        self.right_panel.addWidget(self.local_minima_list)
+
+        # setting up main widget
+        self.main_widget.addWidget(self.left_panel)
         self.main_widget.addWidget(self.plot_widget)
+        self.main_widget.addWidget(self.right_panel)
 
         self.main_widget.setStretchFactor(0, 1)
         self.main_widget.setStretchFactor(1, 3)
 
         self.create_menus()
+
+    def create_left_panel(self):
+        self.left_panel = QSplitter(Qt.Vertical)
+        self.left_panel.setChildrenCollapsible(False)
+
+    def create_right_panel(self):
+        self.right_panel = QSplitter(Qt.Vertical)
+        self.right_panel.setChildrenCollapsible(False)
 
     def create_menus(self) -> None:
         self.create_file_menu()
